44f7e1f720751844870bfad446d930ff154522c2
enam73209 <enam.73209@gmail.com>
2023-11-08 13:30:26 +0600

    initial commit

---
+++ src/Layout/Layout.jsx
@@ -0,0 +1,84 @@
+import React from 'react';
+
+const Layout =(props)=> {
+ 
+        return (
+            <div>
+                <div className="navbar  shadow bg-base-100">
+                    <div className="navbar-start">
+                        <div className="dropdown">
+                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </label>
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                            <li><a>Home</a></li>
+                            <li><a>Service</a></li>
+                            <li><a>About</a></li>
+                            <li><a>Project</a></li>
+                            <li><a>Blog</a></li>
+                            <li><a>Contact</a></li>
+                        </ul>
+                        </div>
+                        <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+                    </div>
+                    <div className="navbar-center hidden lg:flex">
+                        <ul className="menu menu-horizontal px-1">
+                        <li><a>Home</a></li>
+                        <li><a>Service</a></li>
+                        <li><a>About</a></li>
+                        <li><a>Project</a></li>
+                        <li><a>Blog</a></li>
+                        <li><a>Contact</a></li>
+                        </ul>
+                    </div>
+                
+                </div>
+
+                <div>
+                    {props.children}
+                </div>
+
+
+                <div>
+                    <footer className="footer items-center p-4 bg-neutral text-neutral-content">
+                        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:place-self-center">
+                            <aside className="items-center">
+                                <svg
+                                    width="36"
+                                    height="36"
+                                    viewBox="0 0 24 24"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fillRule="evenodd"
+                                    clipRule="evenodd"
+                                    className="fill-current"
+                                >
+                                    {/* Your SVG path here */}
+                                </svg>
+                                <p>Copyright © 2023 - All right reserved</p>
+                            </aside>
+                            <nav className="grid grid-cols-3 gap-4 md:justify-self-end">
+                                <a>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current">
+                                        {/* Your SVG path here */}
+                                    </svg>
+                                </a>
+                                <a>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current">
+                                        {/* Your SVG path here */}
+                                    </svg>
+                                </a>
+                                <a>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current">
+                                        {/* Your SVG path here */}
+                                    </svg>
+                                </a>
+                            </nav>
+                        </div>
+                    </footer>
+                </div>
+            </div>
+        );
+    
+}
+
+export default Layout;
